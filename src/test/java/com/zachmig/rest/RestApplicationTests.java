23901847170f6a234fb0a89ecab0d23361f03355
zmigliorini <zmigliorini@gmail.com>
2023-08-21 21:22:24 -0400

    initial commit

---
+++ src/test/java/com/zachmig/rest/RestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zachmig.rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
